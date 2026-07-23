08246fd8f8fbad0cd05bff0cf894e4273d949dfa
MuhammadUsman0001 <usmannaseem581@gmail.com>
2026-07-23 16:11:48 +0500

    Delete Section8 directory

--- Section8/src/task9a.sv
+++
@@ -1,9 +0,0 @@
-module shift_right_1 (
-    input  logic       shift,
-    input  logic [3:0] W,
-    output logic [3:0] Y,
-    output logic       k
-);
-    assign Y = shift ? {1'b0, W[3:1]} : W;
-    assign k = shift ? W[0] : 1'b0;
-endmodule
